a85bae4c7725fbd5b1ee649cab49f425acf6315e
ZYPROO <shubhamg3203068@gmail.com>
2025-02-12 11:31:47 +0530

    new column added game_page in apps table

--- app.py
+++ app.py
@@ -19,16 +19,23 @@ def get_db_connection():
         port=os.getenv("PGPORT")
     )
 
-# Route 1: Get All Apps
+# Route to get all apps
 @app.route('/apps', methods=['GET'])
 def get_apps():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM apps")
+    query = """
+        SELECT a.id, a.name, a.package_name, a.version, a.icon_url, a.screenshot_url, a.company, a.game_page,
+               dl.link1, dl.link2, dl.link3, dl.link4
+        FROM apps a
+        LEFT JOIN download_links dl ON a.id = dl.app_id
+    """
+    cursor.execute(query)
     apps = cursor.fetchall()
     cursor.close()
     conn.close()
 
+    # Convert results to JSON
     app_list = []
     for app_data in apps:
         app_list.append({
@@ -37,33 +44,53 @@ def get_apps():
             "package_name": app_data[2],
             "version": app_data[3],
             "icon_url": app_data[4],
-            "screenshots": app_data[5].split(',')[:8] if app_data[5] else [],
-            "company": app_data[6]  # If you have the company column
+            "screenshot_url": app_data[5].split(',')[:10],
+            "company": app_data[6],
+            "game_page": app_data[7],
+            "download_links": {
+                "1": app_data[8],
+                "2": app_data[9],
+                "3": app_data[10],
+                "4": app_data[11]
+            }
         })
-
     return jsonify(app_list)
 
-# Route 2: Get Specific App by Package Name
+# Route to get a specific app by package name
 @app.route('/apps/<package_name>', methods=['GET'])
-def get_app_by_package(package_name):
+def get_app_by_package_name(package_name):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM apps WHERE package_name = %s", (package_name,))
+    query = """
+        SELECT a.id, a.name, a.package_name, a.version, a.icon_url, a.screenshot_url, a.company, a.game_page,
+               dl.link1, dl.link2, dl.link3, dl.link4
+        FROM apps a
+        LEFT JOIN download_links dl ON a.id = dl.app_id
+        WHERE a.package_name = %s
+    """
+    cursor.execute(query, (package_name,))
     app_data = cursor.fetchone()
     cursor.close()
     conn.close()
 
     if app_data:
-        app_info = {
+        app_details = {
             "id": app_data[0],
             "name": app_data[1],
             "package_name": app_data[2],
             "version": app_data[3],
             "icon_url": app_data[4],
-            "screenshots": app_data[5].split(',')[:8] if app_data[5] else [],
-            "company": app_data[6]  # Adjust if you have the company column
+            "screenshot_url": app_data[5].split(',')[:10],
+            "company": app_data[6],
+            "game_page": app_data[7],
+            "download_links": {
+                "1": app_data[8],
+                "2": app_data[9],
+                "3": app_data[10],
+                "4": app_data[11]
+            }
         }
-        return jsonify(app_info)
+        return jsonify(app_details)
     else:
         return jsonify({"error": "App not found"}), 404
 
